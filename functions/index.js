6e302ce2dadb63f0d3afa234db14f00cfb53c056
Todd H. Albert <todd@bocacode.com>
2023-04-27 11:53:54 -0400

    working api

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,20 @@
 import functions from 'firebase-functions'
 import express from 'express'
 import cors from 'cors'
+import { login, signup } from './src/users.js'
+import { getShows, addShow } from './src/shows.js'
 
 const app = express() // creates our express app
 app.use(cors())
 app.use(express.json())
 
-// ROUTES GO HERE...
+// User Routes:
+app.post("/signup", signup)
+app.post("/login", login)
 
+// Show Routes
+app.get("/shows", getShows)
+app.post("/shows", addShow)
 
 // lets us run locally without emulators:
 app.listen(3000, () => console.log(`Listening on http://localhost:3000...`))
